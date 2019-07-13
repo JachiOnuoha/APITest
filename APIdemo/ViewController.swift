2adc7bd8b260b58970e4181dd4c62b47d9b7c318
Jachimike Onuoha <jachiuzo@gmail.com>
2019-07-13 16:20:06 -0700

    Initial Commit

---
+++ APIdemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  APIdemo
+//
+//  Created by Jachimike Onuoha on 7/13/19.
+//  Copyright © 2019 Jachimike Onuoha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
