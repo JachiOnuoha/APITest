bcfc777e681351139326e4511a4a806cb8b5bd01
Jachimike Onuoha <jachiuzo@gmail.com>
2019-07-14 01:18:17 -0700

    Working version of Summarizer

--- APIdemo/ViewController.swift
+++ APIdemo/ViewController.swift
@@ -10,21 +10,46 @@ import UIKit
 import algorithmia
 
 class ViewController: UIViewController {
+    
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            // API Key for algorithmia
+            let client = Algorithmia.client(simpleKey: "simxlvnvTyzFJIGg8lagiRLZOmx1")
+            
+            // Reads text file and feeds it into the algorithm to be summarized
+            if let filepath = Bundle.main.path(forResource: "jack_london", ofType: "txt") {
+                do {
+                    let contents = try String(contentsOfFile: filepath)
+                    //print(contents)
+                    let algo = client.algo(algoUri: "nlp/Summarizer/0.1.8")
+                    algo.pipe(text: contents) { resp, error in
+                        print(resp.getText())
+                    }
+                } catch {
+                    print("An error was found")
+                }
+            } else {
+                print("Something else happened")
+            }
+            
+        }
 
-    let input = "A purely peer-to-peer version of electronic cash would allow online payments to be sent directly from one party to another without going through a financial institution. Digital signatures provide part of the solution, but the main benefits are lost if a trusted third party is still required to prevent double-spending. We propose a solution to the double-spending problem using a peer-to-peer network. The network timestamps transactions by hashing them into an ongoing chain of hash-based proof-of-work, forming a record that cannot be changed without redoing the proof-of-work. The longest chain not only serves as proof of the sequence of events witnessed, but proof that it came from the largest pool of CPU power. As long as a majority of CPU power is controlled by nodes that are not cooperating to attack the network, they'll generate the longest chain and outpace attackers. The network itself requires minimal structure. Messages are broadcast on a best effort basis, and nodes can leave and rejoin the network at will, accepting the longest proof-of-work chain as proof of what happened while they were gone.";
-    
-    let client = Algorithmia.client(simpleKey: "simxlvnvTyzFJIGg8lagiRLZOmx1")
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
+}
+
+
+
+
+
+
+
+// Extra code for me to copy and paste later if needed
 //        let nlp_directory = client.dir("data://JachiOnuoha/nlp_directory");
 //
 //        nlp_directory.exists() { exists, error in
 //            if (error == nil){
 //                if (exists == false) {
-//                    nlp_directory.create(readACL:.MY_ALGORITHMS) {error in
+//                    nlp_directory.create(readACL:.PUBLIC) {_, error in
 //                        if (error == nil) {
 //                            print("Folder created")
 //                        } else {
@@ -40,7 +65,7 @@ class ViewController: UIViewController {
 //        nlp_directory.file(name: text_file).exists() { exists, error in
 //            if (error == nil) {
 //                // Check if file exists
-//                let local_file = URL(string: "/Desktop/jack_london")
+//                let local_file = URL(string: "/Desktop/jack_london.txt")
 //                if (exists == false) {
 //                    nlp_directory.put(file: local_file!) { _, error in
 //                        if (error == nil){
@@ -70,8 +95,4 @@ class ViewController: UIViewController {
 //                        }
 //                    }
 //                }
-//            }
-//        }
-
-}
-}
+//            } }
